14d6ba04999f3aed929dc71bc15008a897522d41
Aiman Farhan Hishanudin <ahishanu@outlook.com>
2024-05-17 22:21:35 +0800

    add fuctions for steamspy script

--- etl/flow_steamspy_api_to_csv.py
+++ etl/flow_steamspy_api_to_csv.py
@@ -13,17 +13,26 @@ def get_api_response(url:str):
         pass
     
     return json.loads(response.text)
+
+def to_dataframe(data: dict) -> pd.DataFrame:
+    df = pd.DataFrame(data).T
+    df.reset_index(drop=True)
+    
+    return df
+
+def create_game_list(list: list) -> None:
+    
+    file_path = Path('./games_list.txt')
+    with open(file_path, 'w') as file_gameslist:
+        file_gameslist.writelines(f"{app_id}\n" for app_id in list)
     
 if __name__ == '__main__':
     data = get_api_response(API_URL)
-    df = pd.DataFrame(data).T.reset_index(drop=True)
     
+    df = to_dataframe(data)
     games_list = df['appid'].values.tolist()
     
-    with open('./games_list.txt', 'w') as file_gamelist:
-        for appid in games_list:
-            file_gamelist.write(str(appid) + '\n')
+    create_game_list(games_list)
     
     csv_path = Path('./csv/steamspy_owner_list.csv')
-    
     df.to_csv(csv_path, index_label='ownership_rank')
